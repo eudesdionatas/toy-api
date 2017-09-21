0e7e17b328d1cdd703311f4764dd5f2b13a97f5f
Eudes <eudesdionatas@gmail.com>
2017-09-21 12:23:48 -0300

    Primeiro commit

---
+++ src/main/java/com/eudes/toyapi/toyapi/web/Movie.java
@@ -0,0 +1,16 @@
+package com.eudes.toyapi.toyapi.web;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    private long id;
+    private String title;
+    private String genre;
+    private int year;
+}
---
+++ src/main/java/com/eudes/toyapi/toyapi/web/MoviesController.java
@@ -0,0 +1,34 @@
+package com.eudes.toyapi.toyapi.web;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/movies")
+public class MoviesController {
+
+    private List<Movie> movies = new ArrayList<>();
+    private int idGen = 0;
+
+    public MoviesController() {
+        movies.add(new Movie(++idGen, "Deadpool", "Action", 2016));
+        movies.add(new Movie(++idGen, "Forest Gump", "Comedy", 1990));
+    }
+
+    @PostMapping
+    public ResponseEntity<Movie> save(@RequestBody Movie movie) {
+        // response entity é uma resposta http. vai ter um status (200 - OK, por ex.) e um corpo (opcional)
+        movie.setId(++idGen);
+        movies.add(movie);
+
+        return ResponseEntity.ok(movie);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Movie>> list() {
+        return ResponseEntity.ok(movies);
+    }
+}
